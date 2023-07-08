90e9ac609db200f88edbfce97d4caf74eb7f98d5
Adil <adilvinayak19@gmail.com>
2023-07-08 21:05:31 +0530

    'Routes-set'

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
 import Form from './Components/Form.jsx';
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import {auth} from './firebase-config'
+import {onAuthStateChanged} from 'firebase/auth'
+import { useState } from 'react';
+import Login from './Components/Login';
+import Register from './Components/Register';
+ 
 
 function App() {
+  const [user,setUser]=useState('');
+  onAuthStateChanged(auth,(user1)=>{
+    if(user1){
+      setUser(user1.uid);
+    }
+  })
+
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        {user && <Route path='/' element={<Home />}></Route>}
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/register' element={<Register />}></Route>
+
+      </Routes>
+    </Router>
   );
 }
 
