9c28c815750354db4fb42a1f6d2b0f125f3f02f7
Prachi jain <81459765+Prachijain424@users.noreply.github.com>
2023-07-08 22:29:38 +0530

    home page

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './Home.css';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      <header>
+        <h1>Welcome to My Website</h1>
+      </header>
+      <section>
+        <p>This is the home page of my React.js website.</p>
+      </section>
+      <footer>
+        <p>© 2023 My Website. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
